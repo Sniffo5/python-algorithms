73c683b9de2868fcd9f59f0b0cde76cb345fb287
Aron Loreskär <arolor07301@edu.halmstad.se>
2025-12-01 12:02:23 +0100

    create: algorithm class

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+class Algorithms:
+    __reiterations = 100000
+    
+    def bubble_sort(unsorted_data):
+        pass
+    def selection_sort(unsorted_data):
+        pass
+    def insertion_sort(unsorted_data):
+        pass
+
+unsorted_data = [4,7,9,3,2,8,1,5,6,0]
+
+print(Algorithms.bubble_sort(unsorted_data))
+print(Algorithms.selection_sort(unsorted_data))
