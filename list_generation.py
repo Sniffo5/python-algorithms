4a5a7b3d48557f746fbfbd2b1a6411d2d4a246e8
Aron Loreskär <arolor07301@edu.halmstad.se>
2025-12-05 10:31:58 +0100

    list gen

---
+++ list_generation.py
@@ -0,0 +1,12 @@
+import random
+class Gen:
+    def random(len, min = 1, max= 20000000):
+        return(random.sample(range(min,max),len))
+        
+    def random_semi(len, randomness):
+        pass
+    def random_narrow(len):
+        pass
+    def sorted_reverse(len):
+        
+        pass
