5f44493f5bb9633bb2e6e5147175c634a03ce6f3
Aron Loreskär <aron@loreskar.se>
2026-01-11 22:03:58 +0100

    feat: merge sort

--- algorithms.py
+++ algorithms.py
@@ -37,3 +37,36 @@ class Algorithms:
                     break
             sorted_data[key] = element
         return sorted_data
+    def merge_sort(unsorted_data):
+        if (len(unsorted_data) <= 1):
+            return unsorted_data
+        
+        left_data = unsorted_data[:(len(unsorted_data)//2)]
+        right_data = unsorted_data[(len(unsorted_data)//2):]
+
+        left_sorted = Algorithms.merge_sort(left_data)
+        right_sorted = Algorithms.merge_sort(right_data)
+    
+        return Algorithms.merge(left_sorted, right_sorted)
+    def merge(left_data, right_data):
+        left_index = 0
+        right_index = 0
+        sorted_data = []
+
+        while (left_index < len(left_data) and right_index < len(right_data)):
+            if(left_data[left_index] <= right_data[right_index]):
+                sorted_data.append(left_data[left_index])
+                left_index+=1
+            else:
+                sorted_data.append(right_data[right_index])
+                right_index+=1
+        
+        while left_index < len(left_data):
+            sorted_data.append(left_data[left_index])
+            left_index += 1
+    
+        while right_index < len(right_data):
+            sorted_data.append(right_data[right_index])
+            right_index += 1
+
+        return sorted_data
