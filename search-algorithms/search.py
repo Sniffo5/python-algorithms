57d0295bf1fd3847fba5217791b1ce4f628168f0
Aron Loreskär <arolor07301@edu.halmstad.se>
2026-01-26 13:34:22 +0100

    fix: binary search

--- search-algorithms/search.py
+++ search-algorithms/search.py
@@ -2,51 +2,27 @@ def linear_search(data, search):
     for i in range(len(data)):
         if search == data[i]:
             return i
-def binary_search(data,search):
-    print(data)
-    index = len(data)//2
-    index3 = len(data)// 2
-    index2 = len(data) -1
-    
-    if data[index] == search:
-         return index
-    if(data[index] < search):
-        while True:
-            if data[index] == search:
-                return index
-            if(data[index] < search):
-                index = (index2+index)//2
-            if(data[index] > search):
-                index = (index+index3)//2
-    if(data[index] > search):
-        while True:
-            if data[index] == search:
-                return index
-            if(data[index] < search):
-                index = (index3+index)//2
-            if(data[index] > search):
-                index = (index)//2
-                
-                
+
+
 def binary_search2(data ,search):
     low = 0
     high = len(data) -1
+    index = (high-low)//2
     
-    while low <= high:
-        index = (high-low)//2
-        
+    while low <= high and high -1 != low:
         if data[index] == search:
             return index
         if data[index] < search:
             low = index
+            index = (high+low)//2
         if data[index] > search:
             high = index
-            
-            """ add exception for when number does not exist """
+            index = (high-low)//2
+    return -1
     
 
         
-lista = [1,1,6,7,7,8,8,9,9,10]
+lista = [1,2,3,4,5,6,7,8,9,10]
 
 print(linear_search(lista[:],5))
-print(binary_search2(lista[:],5))
+print(binary_search2(lista[:],0))
